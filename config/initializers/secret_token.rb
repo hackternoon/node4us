0178af3b55b578840a9800424445b9a83c4f218b
Dan Bikle <dan@bot4.us>
2012-04-09 22:39:51 +0100

    Its_alive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+N4::Application.config.secret_token = '93183e8142a867765eb5955f605268bda2990781c386418d6fa4e4a09ea8e3853ba4c761ec8b8ae2a105df82302bdb64b2859f80cc2f84515d9d9564026339ed'
